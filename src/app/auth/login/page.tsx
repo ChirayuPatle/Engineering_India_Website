97a8c62a389f4b0cddc1c2450b7f41624ac50826
Priyanshudotdev <priyanshu.kayarkar009@gmail.com>
2025-02-07 22:35:34 +0530

    feat: add-auth-integration

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>Login</div>;
-};
+import { userService } from "@/libs/appwrite/config";
+import { FaGoogle } from "react-icons/fa";
 
-export default page;
+export default function SignIn() {
+  const handleUserLogin = async () => {
+    const user = userService.loginWithGoogle();
+  };
+
+  return (
+    <div className="flex min-h-screen">
+      <div className="hidden lg:flex w-1/2 bg-gradient-to-br from-blue-600 to-blue-500 justify-center items-center p-12">
+        <div>
+          <h1 className="text-5xl font-bold leading-tight text-white">
+            Welcome to Our Community!
+          </h1>
+
+          <p className="mt-4 text-xl text-blue-200">
+            Join thousands of innovators building together.
+          </p>
+        </div>
+      </div>
+
+      <div className="w-full lg:w-1/2 flex justify-center items-center bg-white p-6">
+        <div className="w-full max-w-md text-center ">
+          <h2 className="text-gray-900 text-3xl font-bold mb-8">
+            Get's Started !
+          </h2>
+
+          <button
+            onClick={() => handleUserLogin()}
+            className="w-full flex gap-4 items-center justify-center p-3 bg-blue-600 text-white rounded-lg font-semibold hover:bg-blue-700 transition"
+          >
+            <FaGoogle className="text-xl" />
+            Sign In with Google
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
 "use client";
 
+import Gallery from "@/features/landing/components/gallery";
 import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
-export default function HeroSection() {
+export default function Page() {
+  const router = useRouter();
   return (
     <section className="relative w-full min-h-screen overflow-hidden">
       <main className="container mx-auto px-6 h-screen flex items-center justify-center relative gap-12">
         {/* Background Blurs */}
         <div className="absolute top-0 left-0 w-72 h-72 bg-blue-200 rounded-full opacity-60 blur-3xl -translate-x-1/2 -translate-y-1/2" />
-        <div className="absolute -bottom-[20%] left-1/4 w-[60%] h-[50%] bg-blue-400 rounded-full opacity-40 blur-3xl translate-x-[2%] translate-y-1/4" />
-        <div className="absolute top-[40%] left-[40%] w-[70%] h-[50%] bg-blue-primary rounded-full opacity-20 blur-3xl -translate-x-1/2 -translate-y-1/2" />
+        <div className="absolute -bottom-[20%] left-1/4 w-[60%] h-[s0%] bg-blue-400 rounded-full opacity-40 blur-3xl translate-x-[2%] translate-y-1/4" />
+        <div className="absolute top-[40%] left-[40%] w-[65%] h-[35%] bg-blue-primary rounded-full opacity-20 blur-3xl -translate-x-1/2 -translate-y-1/2" />
 
         {/* Left Content */}
-        <div className="relative z-10 max-w-2xl ">
+        <div className="relative z-10 max-w-2xl">
           <h1 className="text-5xl font-bold text-gray-900 leading-tight mb-6">
             Engineering India | YCCE
           </h1>
           <p className="text-lg text-gray-600 mb-8">Club Tagline</p>
           <div className="flex gap-4">
-            <button className="px-6 py-3 text-white bg-blue-600 rounded-full shadow-lg hover:bg-blue-700 transition-colors">
+            <button
+              onClick={() => router.push("/auth/login")}
+              className="px-6 py-3 text-white bg-blue-600 rounded-full shadow-lg hover:bg-blue-700 transition-colors"
+            >
               Get Started
             </button>
             <button className="px-6 py-3 text-blue-600 bg-blue-100 hover:bg-white border border-blue-600 rounded-full shadow-lg transition-colors">
@@ -27,7 +33,6 @@ export default function HeroSection() {
           </div>
         </div>
 
-        {/* Right Side - Animated Image Grid */}
         <motion.div
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -35,8 +40,9 @@ export default function HeroSection() {
           className="relative z-10 flex-shrink-0"
         ></motion.div>
       </main>
+      <main className="min-h-screen"></main>
       <main className="container mx-auto px-6 h-screen flex items-center justify-center relative gap-12">
-        {/* Background Blurs */}
+        <Gallery />
       </main>
     </section>
   );
