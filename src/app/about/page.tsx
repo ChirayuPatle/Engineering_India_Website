88266099c18f1426f6a89480cce26c72e932fc92
Priyanshudotdev <priyanshu.kayarkar009@gmail.com>
2025-02-26 13:28:13 +0530

    fix: git conflicts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -79,9 +79,7 @@ export default function AboutPage() {
                 <Typography variant="h3" className="mb-2">
                   {item.title}
                 </Typography>
-                <Typography className="">
-                  {item.description}
-                </Typography>
+                <Typography className="">{item.description}</Typography>
               </div>
             ))}
           </div>
