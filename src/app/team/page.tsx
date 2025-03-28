2648e1b5b3100d658b583c21084b272305a6b7fb
MUCHKUNDRAJE <muchkundraje.thote@vectorlab.in>
2025-03-28 23:37:05 +0530

    add team

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -19,17 +19,19 @@ const TeamCard: React.FC<TeamCardProps> = ({
   return (
     <div
       onClick={onClick}
-      className="perspective-1000 hover:rotate-x-3 hover:rotate-y-3 group relative flex h-96 w-72 transform cursor-pointer flex-col items-center rounded-2xl border border-gray-300 bg-gray-100 p-6 shadow-xl transition-all duration-500 ease-out hover:shadow-2xl md:mt-10"
+      className="perspective-1000 hover:rotate-x-3 hover:rotate-y-3 group relative flex h-96 w-64 transform cursor-pointer flex-col items-center rounded-2xl border border-gray-300 bg-gray-100 p-6 shadow-xl transition-all duration-500 ease-out hover:shadow-2xl md:mt-10"
     >
       {/* Background Gradient */}
       <div className="absolute inset-0 rounded-2xl bg-gradient-to-br from-orange-500 to-green-500 opacity-20 transition-opacity duration-500 group-hover:opacity-40" />
 
       {/* Profile Image */}
+      <div  className="w-44 flex items-center justify-center ">
       <img
         src={image}
         alt="Profile"
         className="mt-6 h-36 w-36 rounded-full border-4 border-black object-cover p-1 shadow-lg"
       />
+      </div>
 
       {/* Member Details */}
       <div className="relative z-10 mt-4 text-center">
@@ -44,12 +46,12 @@ const TeamPage: React.FC = () => {
   const router = useRouter();
   return (
     <>
-      <div>
+      <div className="w-full h-full ">
         <h1 className="-mb-10 mt-20 text-center text-4xl font-bold text-zinc-600">
           Our Team Leads
         </h1>
-        <div className="mt-[2rem] flex flex-wrap justify-center gap-6 p-10">
-          <div className="my-[4rem]"></div>
+        <div className=" lg:ml-0 mt-[2rem] flex items-start  md:flex-wrap  overflow-auto md:overflow-hidden  justify-start  md:justify-center gap-6 p-10">
+       
           {teamMembers.map((details, index) => (
             <TeamCard
               onClick={() => router.push(`/team-info/leads/${details.teamId}`)}
@@ -63,10 +65,10 @@ const TeamPage: React.FC = () => {
         </div>
       </div>
       <div>
-        <h1 className="-mb-10 mt-20 text-center text-4xl font-bold text-zinc-600">
+        <h1 className="-mb-10 mt-20 text-center text-3xl font-bold text-zinc-600  lg:text-4xl">
           2rd Year Coordinators
         </h1>
-        <div className="mt-[2rem] flex flex-wrap justify-center gap-6 p-10">
+        <div className=" lg:ml-0 mt-[2rem] flex items-start  md:flex-wrap  overflow-auto md:overflow-hidden  justify-start  md:justify-center gap-6 p-10">
           <div className="my-[4rem]"></div>
           {SecondYear.map((details, index) => (
             <TeamCard
@@ -81,10 +83,10 @@ const TeamPage: React.FC = () => {
         </div>
       </div>
       <div>
-        <h1 className="-mb-10 mt-20 text-center text-4xl font-bold text-zinc-600">
+        <h1 className="-mb-10 mt-20 text-center text-3xl lg:text-4xl  font-bold text-zinc-600">
           3rd Year Coordinators
         </h1>
-        <div className="mt-[2rem] flex flex-wrap justify-center gap-6 p-10">
+        <div className=" lg:ml-0 mt-[2rem] flex items-start  md:flex-wrap  overflow-auto md:overflow-hidden  justify-start  md:justify-center gap-6 p-10">
           <div className="my-[4rem]"></div>
           {ThridYear.map((details, index) => (
             <TeamCard
@@ -102,7 +104,7 @@ const TeamPage: React.FC = () => {
         <h1 className="-mb-10 mt-20 text-center text-4xl font-bold text-zinc-600">
           Devlopers
         </h1>
-        <div className="mt-[2rem] flex flex-wrap justify-center gap-6 p-10">
+        <div className=" lg:ml-0 mt-[2rem] flex items-start  md:flex-wrap  overflow-auto md:overflow-hidden  justify-start  md:justify-center gap-6 p-10">
           <div className="my-[4rem]"></div>
           {Devlopers.map((details, index) => (
             <TeamCard
